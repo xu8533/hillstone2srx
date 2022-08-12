071ec50642f25f15043e8c4d72e45ff2a17eb830
Xu Jian <838178533@qq.com>
2022-08-12 21:27:08 +0800

    Add files via upload
    translate of address, service, servgroup, rule added

--- hilston2srx.pl
+++ hilston2srx.pl
@@ -3,15 +3,15 @@ use warnings;
 #use strict;
 use Data::Dumper;
 use Scalar::Util qw(looks_like_number);
-use NetAddr::IP;
-use Net::IP::LPM;
+#use NetAddr::IP;
+#use Net::IP::LPM;
 use Getopt::Std;
 use Cwd 'abs_path';
 use File::Basename;
-use Excel::Writer::XLSX;
-use vars qw($opt_c);
+#use Excel::Writer::XLSX;
+#use vars qw($opt_c);
 use v5.10.1;
-use DateTime::Format::Flexible;
+#use DateTime::Format::Flexible;
 #The major aim of the script is translate juniper's ssg config to juniper srx config
 
 # define variable
@@ -20,55 +20,123 @@ my $DST_ZONE;
 my @SRC_ADDRESSES;
 my @DST_ADDRESSES;
 my $POLICY_ACTION;
-my $text;
 my $GLOBAL_ADDRESS_BOOKS;
+my $text;
+my @texts;
 my $n=0;
 my %hilston_srx_services        = (
-    FTP     => "junos-ftp",
+    FTP     => "junos-ftp",		Any		=> "any",
     HTTP    => "junos-http",    HTTPS   => "junos-https",
-    IKE     => "junos-ike",     IMAP    => "junos-imap",
-    LDAP    => "junos-ldap",    MSN     => "junos-msn",
-    MAIL    => "junos-mail",    NBDS    => "junos-nbds",
-    NBNAME  => "junos-nbname",  NTP     => "junos-ntp",
-    PING    => "junos-ping",    POP3    => "junos-pop3",
-    PPTP    => "junos-pptp",    RADIUS  => "junos-radius",
-    RTSP    => "junos-rtsp",
-    RSH     => "junos-rsh",     SIP     => "junos-sip",
-    SMTP    => "junos-smtp",    SMB     => "junos-smb",
     SSH     => "junos-ssh",     SYSLOG  => "junos-syslog",
-    TFTP    => "junos-tftp",    TELNET  => "junos-telnet",
-    WHOIS   => "junos-whois",   WINFRAME    => "junos-winframe",
-    'ICMP-ANY'      => "junos-ping",
-    'ICMP-any'      => "junos-ping",
-    'HTTP-EXT'      => "junos-http-ext",
-    'Real-Media'    => "junos-realaudio",
-    'Real Media'    => "junos-realaudio",
-    'SQL\*Net_V1'   => "junos-sqlnet-v1",
-    'SQL\*Net_V2'   => "junos-sqlnet-v2",
-    'SQL\*Net V1'   => "junos-sqlnet-v1",
-    'SQL\*Net V2'   => "junos-sqlnet-v2",
-    'SQL Monitor'   => "junos-sql-monitor",
-    'X-WINDOWS'     => "junos-x-windows",
-    "H.323"         => "junos-h323",
-    'Internet Locator Service'  => "junos-internet-locator-service",
-);
-my %srx_application_port_number = (
-    http    => 80,     https    => 443,     ftp     => 21,
-    ssh     => 22,     mail     => 25,      telnet  => 23,
-    Terminal => 3389,  SNMP    => "161 to 162",
+	RDP		=> "junos-rdp",
 );
 
 sub set_address_books {
-	my $address_book_name = @_;
-	for 
+	local $address_book_name = "@_";
+	$n++;
+	until($texts[$n] eq "exit") {
+		local @cells = split/\s+/, $texts[$n];
+		given($cells[0]) {
+			when ("ip") {
+				print "set security address-book global address $address_book_name $cells[-1]\n";
+			}
+			when ("range") {
+				print "set security address-book global address $address_book_name range-address $cells[-2] to $cells[-1]\n";
+			}
+			when ("description") {
+				print "set security address-book global address $address_book_name description $cells[-1]\n";
+			}
+		}	
+		$n++;
+	}
+	return;
 }
 
 sub set_services {
-
+	local ($service_type, $service_name) = @_;	
+	$n++;
+	if ($service_type eq "service") {
+		until ($texts[$n] eq "exit" ) {
+			local @cells = split/\s+/, $texts[$n];
+			local $cells_num = @cells;
+			if ($cells_num == 3) {
+				print "set applications application $service_name term $cells[0]-$cells[1]-$cells[2] protocol $cells[0] destination-port $cells[2]\n"; 
+			}
+			elsif ($cells_num == 6) {
+				print "set applications application $service_name term $cells[0]-$cells[1]-$cells[2] protocol $cells[0] destination-port $cells[2] source-port $cells[-2]-$cells[-1]\n";
+			}
+			elsif ($cells_num == 7) {
+				print "set applications application $service_name term $cells[0]-$cells[1]-$cells[2]-$cells[3] protocol $cells[0] destination-port $cells[2]-$cells[3] source-port $cells[-2]-$cells[-1]\n";
+			}
+			elsif ($cells_num == 4) {
+				print "set applications application $service_name term $cells[0]-$cells[1]-$cells[2]-$cells[3] protocol $cells[0] destination-port $cells[-2]-$cells[-1]\n"; 
+			}
+			$n++;
+		}
+	}
+	elsif ($service_type eq "servgroup") {
+		until ($texts[$n] eq "exit") {
+			local @cells = split/\s+/, $texts[$n];
+			print "set applications application-set $service_name application $cells[-1]\n";
+			$n++;
+		}
+	}
+	return;
 }
 
 sub set_polices {
-
+	local $policy_id = "@_";
+	$n++;
+	local ($action, $src_zone, $dst_zone, @src_address, @dst_address, @application);
+	until($texts[$n] eq "exit") {
+		local @cells = split/\s+/, $texts[$n];
+		given($cells[0]) {
+			when ("action") {
+				$action = $cells[-1];	
+			}
+			when ("src-zone") {
+				$src_zone = $cells[-1];
+			}
+			when ("dst-zone") {
+				$dst_zone = $cells[-1];
+			}
+			when ("src-addr") {
+				push @src_address, $cells[-1];
+			}
+			when ("dst-addr") {
+				push @dst_address, $cells[-1];
+			}
+			when ("src-ip") {
+				push @src_address, $cells[-1];
+				print "set security address-book global address $cells[-1] $cells[-1]\n";
+			}
+			when ("dst-ip") {
+				push @dst_address, $cells[-1];
+				print "set security address-book global address $cells[-1] $cells[-1]\n";
+			}
+			when ("service") {
+				push @application, $cells[-1];
+			}
+			when ( "src-range") {
+				push @src_address, "range-$cells[-2]-$cells[-1]";
+				print "set security address-book global address range-$cells[-2]-$cells[-1] range-address $cells[-2] to $cells[-1]\n";
+			}
+			when ( "dst-range") {
+				push @dst_address, "range-$cells[-2]-$cells[-1]";
+				print "set security address-book global address range-$cells[-2]-$cells[-1] range-address $cells[-2] to $cells[-1]\n";
+			}
+		}
+		$n++;
+	}
+	if (defined ($src_zone && $dst_zone)) {
+		print "set security policies from-zone $src_zone to-zone $dst_zone policy p_$policy_id match source-address [ @src_address ] destination-address [ @dst_address ] application [ @application ]\n";
+		print "set security policies from-zone $src_zone to-zone $dst_zone policy p_$policy_id then $action\n";
+	}
+	elsif (!defined ($src_zone && $dst_zone)) {
+		print "set security policies global policy p_$policy_id match source-address [ @src_address ] destination-address [ @dst_address ] application [ @application ]\n";
+		print "set security policies global policy p_$policy_id then $action\n";
+	}
+	return;
 }
 
 #The BEGIN part process some staff
@@ -86,6 +154,8 @@ BEGIN {
     # save all content of config to a variable, we will process the variable instead of <>
     open my $config, '<', $ARGV[0] or die "can't open file:$!\n"; #open the config filehandle
     $text = do { local $/; <$config> };
+	$text =~ s#\"##g;
+	#	$text =~ s/^\s+//g;
     close $config;
 }
 
@@ -94,71 +164,32 @@ while (($key, $value) = each %hilston_srx_services) {
     $text =~ s/\b$key\b/$value/gm;
 }
 
-my @texts = split(/\n/, $text);
-@texts =~ s#\"##g;
+@texts = split(/\n/, $text);
 
-for $n++ (@texts) {
-    my @configs = split/\s+/;
+# remove blank lines
+@texts = grep { !/(^$|^\n$|^\s+$)/ } @texts;
+
+# remove white at begein and end
+@texts = map { s/^\s+|\s+$//gr } @texts;
+
+while ($texts[$n]) {
+    my @configs = split/\s+/, $texts[$n];
 	given($configs[0]) {
 		when ("address") {
 			set_address_books ($configs[-1]);		
 		}
-		when ("service" || "servgroup") {
-			set_services ($configs[-1]);
+		when ($_ eq "service" || $_ eq "servgroup") {
+			set_services ($configs[0], $configs[-1]);
 		}
 		when ("rule") {
 			set_polices ($configs[-1]);
 		}
 	}
+	$n++;
 }
 
 # the last jobs
 END {
-    #replace nat port name with port number
-    while (my ($junos_app, $real_port_number) = each %srx_application_port_number) {
-        map { s/\b$junos_app\b/$real_port_number/g } @destination_nat;
-        map { s/\b$junos_app\b/$real_port_number/g } @source_nat;
-    }
-    #merge all address books
-    local @all_address_books_tmp 
-        = (@dst_ip_address_books,
-           @global_address_books,
-           @mip_address_books
-        ); 
-    #my @source_nat_rule_set_direction = keys { map { $_ => 1 } @source_nat_zone };
-    #my @destination_nat_rule_set_direction = keys { map { $_ => 1 } @destination_nat_zone };
-    #remove duplicate source and destination nat rule-set condition 
-    my @source_nat_rule_set_direction 
-        = do { my %tmp_src;
-               grep { !$tmp{$_}++ } @source_nat_zone
-             }
-        ;
-    my @destination_nat_rule_set_direction 
-        = do { my %tmp_dst;
-               grep { !$tmp{$_}++ } @destination_nat_zone 
-             }
-        ;
-    local @all_address_books 
-        = do { my %tmp_all_address_books;
-               grep { !$tmp_all_address_books{$_}++ } 
-               @all_address_books_tmp
-             }
-        ;
-    @source_nat 
-        = grep !/\bdestination-port ping\b/, @source_nat;
-    @destination_nat
-        = grep !/\bdestination-port ping\b/, @destination_nat;
-    foreach (@source_nat) {
-        $_ =~ s!\bany\b!0.0.0.0/0!g;
-    }
-    foreach (@destination_nat) {
-        $_ =~ s!\bany\b!0.0.0.0/0!g;
-    }
-    print @source_nat_rule_set_direction;
-    print @source_nat;
-    print @destination_nat_rule_set_direction;
-    print @destination_nat;
-    print @all_address_books;
     print "set applications application traceroute-icmp term t1 protocol icmp\n";
     print "set applications application traceroute-icmp term t1 icmp-type 8\n";
     print "set applications application traceroute-icmp term t1 icmp-code 0\n";
